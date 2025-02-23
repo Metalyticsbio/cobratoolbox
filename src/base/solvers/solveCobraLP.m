c97351eee44cbf0763a3f556cc26765b01d815de
Greg <greg.perry@metalyticsbio.com>
2025-02-23 07:46:11 -0800

    solver = 'glpk';

--- src/base/solvers/solveCobraLP.m
+++ src/base/solvers/solveCobraLP.m
@@ -223,6 +223,7 @@ origStatText = [];
 algorithm = 'default';
 
 t_start = clock;
+solver = 'glpk';
 if isempty(solver)
     error('Default LP solver not selected. Initialise the COBRA Toolbox with: initCobraToolbox')
 end
